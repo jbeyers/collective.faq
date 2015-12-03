2ef1fc0cd850913c1f0934109d275e8bf04125cb
Johan Beyers <johan@juizi.com>
2015-12-03 06:57:14 +0200

    Create default view

--- collective/faq/faq.py
+++ collective/faq/faq.py
@@ -61,22 +61,10 @@ class FAQ(Item):
     pass
 
 
-# View class
-# The view will automatically use a similarly named template in
-# faq_templates.
-# Template filenames should be all lower case.
-# The view will render when you request a content object with this
-# interface with "/@@sampleview" appended.
-# You may make this the default view for content objects
-# of this type by uncommenting the grok.name line below or by
-# changing the view class name and template filename to View / view.pt.
-
-class SampleView(grok.View):
+class View(grok.View):
     """ sample view class """
 
     grok.context(IFAQ)
     grok.require('zope2.View')
-
-    # grok.name('view')
 
     # Add view methods here
